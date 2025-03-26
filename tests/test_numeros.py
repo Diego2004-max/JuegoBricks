422e820dfa2b51eaa3dd34aa76f30ad8ee1a2db4
Diego Mallama <diego.mallamay@campusucc.edu.co>
2025-03-26 00:17:09 -0500

    Update test_numeros.py

--- tests/test_numeros.py
+++ tests/test_numeros.py
@@ -1,1 +1,9 @@
+from src.numeros import es_primo
+
+def test_es_primo():
+    assert es_primo(2) is True
+    assert es_primo(3) is True
+    assert es_primo(4) is False
+    assert es_primo(11) is True
+    assert es_primo(25) is False
 
